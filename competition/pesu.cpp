16b89fc090f1041163196a4e4d777cd1c084e4ad
Uptaker <markus.tammeoja@gmail.com>
2022-10-13 22:56:02 +0300

    maybe it'll be faster with global variables?

--- competition/pesu.cpp
+++ competition/pesu.cpp
@@ -8,17 +8,28 @@ enum Task {
     FLIP, FRONT, BACK, RED, GREEN, BLUE
 };
 
-void colorCounter(int start, int end, char *arr, char color, int size) {
+map<string, Task> mappedTask = {
+    {"POORA", FLIP},
+    {"ETTE", FRONT},
+    {"TAHA", BACK},
+    {"PUNASEID", RED},
+    {"ROHELISI", GREEN},
+    {"SINISEID", BLUE},
+};
+
+int shirtAmount, queryAmount, startAt, endAt, i;
+
+void colorCounter(char *arr, char color) {
     int count = 0;
-    for (int i = start; i < end + 1; i++) {
+    for (int i = startAt; i < endAt + 1; i++) {
         if (arr[i] == color) count++;
     }
     cout << count << endl;
 }
 
-void flip(int start, int end, char *arr) {
+void flip(char *arr) {
     char temp;
-    for (int low = start, high = end; low < high; low++, high--) {
+    for (int low = startAt, high = endAt; low < high; low++, high--) {
         temp = arr[low];
         arr[low] = arr[high];
         arr[high] = temp;
@@ -26,7 +37,6 @@ void flip(int start, int end, char *arr) {
 }
 	
 int main(void) {
-    int shirtAmount, queryAmount;
 	cin >> shirtAmount >> queryAmount;
 
     char shirts[shirtAmount];
@@ -34,49 +44,42 @@ int main(void) {
     cin >> shirts;
 
     // would be cool to have something like a map of <string, function()>.. if I could figure it out
-    map<string, Task> mappedTask = {
-        {"POORA", FLIP},
-        {"ETTE", FRONT},
-        {"TAHA", BACK},
-        {"PUNASEID", RED},
-        {"ROHELISI", GREEN},
-        {"SINISEID", BLUE},
-    };
+
     
     string command;
     char temp;
-    int shirtL, shirtR, start, end, i;
+    int shirtL, shirtR;
     for (int j = 0; j < queryAmount; j++) {
 		cin >> command  >> shirtL >> shirtR;
-        start = shirtL - 1;
-        end = shirtR - 1;
+        startAt = shirtL - 1;
+        endAt = shirtR - 1;
 
         switch(mappedTask[command]) {
             case FLIP:
-                flip(start, end, shirts);
+                flip(shirts);
                 break;
             case FRONT:
-                for (i = 0; i < end - start + 1; i++) {
+                for (i = 0; i < endAt - startAt + 1; i++) {
                     temp = shirts[i];
-                    shirts[i] = shirts[start + i];
-                    shirts[start + i] = temp;
+                    shirts[i] = shirts[startAt + i];
+                    shirts[startAt + i] = temp;
                 }
                 break;
             case BACK:
-                for (i = 0; i < end - start + 1; i++) {
-                    temp = shirts[start + i];
-                    shirts[start + i] = shirts[start + i + (shirtAmount - shirtR)];
-                    shirts[start + i + (shirtAmount - shirtR)] = temp;
+                for (i = 0; i < endAt - startAt + 1; i++) {
+                    temp = shirts[startAt + i];
+                    shirts[startAt + i] = shirts[startAt + i + (shirtAmount - shirtR)];
+                    shirts[startAt + i + (shirtAmount - shirtR)] = temp;
                 }
                 break;
             case RED:
-                colorCounter(start, end, shirts, 'P', shirtAmount);
+                colorCounter(shirts, 'P');
                 break;
             case GREEN:
-                colorCounter(start, end, shirts, 'R', shirtAmount);
+                colorCounter(shirts, 'R');
                 break;
             case BLUE:
-                colorCounter(start, end, shirts, 'S', shirtAmount);
+                colorCounter(shirts, 'S');
                 break;
         };
     }
