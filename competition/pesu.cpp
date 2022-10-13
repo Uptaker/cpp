5b5fc5d456f4ac40a5f63bf3531f5ade4f754e57
Uptaker <markus.tammeoja@gmail.com>
2022-10-13 22:43:38 +0300

    initialize most vacriables only once

--- competition/pesu.cpp
+++ competition/pesu.cpp
@@ -10,43 +10,18 @@ enum Task {
 
 void colorCounter(int start, int end, char *arr, char color, int size) {
     int count = 0;
-    for (int i = start; i <= end; i++) {
-        char col = arr[i];
+    for (int i = start; i < end + 1; i++) {
         if (arr[i] == color) count++;
     }
     cout << count << endl;
 }
 
 void flip(int start, int end, char *arr) {
+    char temp;
     for (int low = start, high = end; low < high; low++, high--) {
-        int temp = arr[low];
+        temp = arr[low];
         arr[low] = arr[high];
         arr[high] = temp;
-    }
-}
-
-void shift_left(char arr[], int size, int numTimes) {
-    while (numTimes) {
-        char temp = arr[0];
-        arr[size - 1] = temp;
-        for (int i = 0; i < (size - 1); i++) {
-            arr[i] = arr[i + 1];
-        }
-        arr[size - 1] = temp;
-        numTimes--;
-    }
-
-}
-
-void shift_right(char arr[], int size, int numTimes) {
-    while (numTimes) {
-        char temp = arr[size - 1];
-        arr[size - 1] = temp;
-        for (int i = size - 1; i > 0; i--) {
-            arr[i] = arr[i - 1];
-        }
-        arr[0] = temp;
-        numTimes--;
     }
 }
 	
@@ -55,6 +30,7 @@ int main(void) {
 	cin >> shirtAmount >> queryAmount;
 
     char shirts[shirtAmount];
+    
     cin >> shirts;
 
     // would be cool to have something like a map of <string, function()>.. if I could figure it out
@@ -67,28 +43,28 @@ int main(void) {
         {"SINISEID", BLUE},
     };
     
+    string command;
+    char temp;
+    int shirtL, shirtR, start, end, i;
     for (int j = 0; j < queryAmount; j++) {
-    	string command;
-    	int shirtL, shirtR, start, end;
 		cin >> command  >> shirtL >> shirtR;
         start = shirtL - 1;
         end = shirtR - 1;
-
 
         switch(mappedTask[command]) {
             case FLIP:
                 flip(start, end, shirts);
                 break;
             case FRONT:
-                for (int i = 0; i < end - start + 1; i++) {
-                    char temp = shirts[i];
+                for (i = 0; i < end - start + 1; i++) {
+                    temp = shirts[i];
                     shirts[i] = shirts[start + i];
                     shirts[start + i] = temp;
                 }
                 break;
             case BACK:
-                for (int i = 0; i < end - start + 1; i++) {
-                    char temp = shirts[start + i];
+                for (i = 0; i < end - start + 1; i++) {
+                    temp = shirts[start + i];
                     shirts[start + i] = shirts[start + i + (shirtAmount - shirtR)];
                     shirts[start + i + (shirtAmount - shirtR)] = temp;
                 }
