c81ec228a325658ba79bc194c43e121f9de274e5
Markus Tammeoja <markus@codeborne.com>
2022-12-05 10:45:08 +0200

    multiply, plus, array index overloading examples

--- design_patterns/operator_overloading.cpp
+++ design_patterns/operator_overloading.cpp
@@ -17,17 +17,45 @@ class Vektor {
 
     friend ostream& operator<<(ostream& os, const Vektor& v);
 
+    Vektor operator*(int arv) {
+        return Vektor(x* arv, y*arv);
+    }
+
+    Vektor operator+(Vektor v) {
+        return Vektor(x + v.x, y + v.y);
+    }
+
+    int operator[](int koht) {
+        if (koht == 1) return x;
+        if (koht == 2) return y;
+        return -1;
+    }
+
 };
 
 ostream& operator<<(ostream& os, const Vektor& v) {
     return os << "(" << v.x << "," << v.y << ")";
 }
 
+
+
 int main(void) {
     Vektor v(3, 8);
+
+    Vektor v2 = v * 2;
+
+    Vektor v3 = v + v2;
 
     v.paremale();
 
     cout << v << endl;
+    cout << v2 << endl;
+    cout << v3 << endl;
+    cout << v3[1] << endl;
+    cout << v3[2] << endl;
+    cout << v3[3] << endl;
+
+
+
 }
 
