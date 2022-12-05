18dae18cd2095fd10f6689c516f665b183e562ed
Markus Tammeoja <markus@codeborne.com>
2022-12-05 10:32:15 +0200

    add operator overloading example

---
+++ design_patterns/operator_overloading.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+
+using namespace std;
+class Vektor {
+    int x, y;
+    public:
+    Vektor(int x, int y) {
+        this->x = x;
+        this->y = y;
+    }
+
+    int getX() { return x; }
+
+    int getY() { return y; }
+
+    void paremale() { x++; }
+
+    friend ostream& operator<<(ostream& os, const Vektor& v);
+};
+
+ostream& operator<<(ostream& os, const Vektor& v) {
+    return os << "(" << v.x << "," << v.y << ")";
+}
+
+int main(void) {
+    Vektor v(3, 8);
+
+    v.paremale();
+
+    cout << v << endl;
+}
+
