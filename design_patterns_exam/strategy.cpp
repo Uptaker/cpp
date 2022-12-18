6b0125d71ac179f1195354b7ecbde88cf7f3a784
Markus Tammeoja <markus@codeborne.com>
2022-12-18 20:06:10 +0200

    add strategy pattern

---
+++ design_patterns_exam/strategy.cpp
@@ -0,0 +1,54 @@
+/*
+
+Design pattern that enables an algorithm's behavior to be selected at runtime.
+This allows for greater flexibility in the implementation of algorithms and the ability to easily swap one algorithm for another.
+
+*/
+
+#include <iostream>
+
+using namespace std;
+
+class PaymentGateway {
+public:
+    virtual void processPayment(double amount) = 0;
+};
+
+class PayPal : public PaymentGateway {
+public:
+    void processPayment(double amount) override {
+        cout << "paying using PayPal API" << endl;
+    }
+};
+
+class LHV : public PaymentGateway {
+public:
+    void processPayment(double amount) override {
+        cout << "paying using LHV API" << endl;
+
+    }
+};
+
+class Swedbank : public PaymentGateway {
+public:
+    void processPayment(double amount) override {
+        cout << "paying using Swedbank API" << endl;
+    }
+};
+
+void makePayment(PaymentGateway& gateway, double amount) {
+    gateway.processPayment(amount);
+}
+
+int main() {
+    PayPal paypal;
+    makePayment(paypal, 100.00);
+
+    LHV lhv;
+    makePayment(lhv, 50.00);
+
+    Swedbank swed;
+    makePayment(swed, 50.00);
+
+    return 0;
+}
